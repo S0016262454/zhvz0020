9a2dddbd5a74855e474bf0a453879eeab48f0d5f
S0016262454 <akitaka.shimizu@ips.ne.jp>
2016-12-09 09:05:59 +0000

    v1.0.2

--- webapp/controller/zhvz0020_0010.controller.js
+++ webapp/controller/zhvz0020_0010.controller.js
@@ -1,98 +1,103 @@
-jQuery.sap.registerModulePath("zhyi0010", "/zhyi0010"); 
+jQuery.sap.registerModulePath("zhyi0010", "/zhyi0010");
 jQuery.sap.require("zhyi0010.commons.zhyi0010_0010_commons");
 
 jQuery.sap.includeStyleSheet('/zhyi0010/css/zhyi0010_0010.css');
 
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-		'sap/m/Button',
-		'sap/m/Dialog',
-		'sap/m/MessageToast',
-		'sap/m/Text',
-		'sap/m/TextArea',
-		'sap/ui/layout/HorizontalLayout',
-		'sap/ui/layout/VerticalLayout',
-		'sap/ui/model/json/JSONModel',
-		'sap/ui/core/util/Export',
-		'sap/ui/core/util/ExportTypeCSV',
-		'zhvz0020/model/Formatter',
-	"zhyi0010/commons/zhyi0010_0010_commons"
-], function(Controller, Button, Dialog, MessageToast, Text, TextArea, HorizontalLayout, VerticalLayout, JSONModel, Export, ExportTypeCSV, Formatter, ommons) {
+	'sap/m/Button',
+	'sap/m/Dialog',
+	'sap/m/MessageToast',
+	'sap/m/Text',
+	'sap/m/TextArea',
+	'sap/ui/layout/HorizontalLayout',
+	'sap/ui/layout/VerticalLayout',
+	'sap/ui/model/json/JSONModel',
+	'sap/ui/core/util/Export',
+	'sap/ui/core/util/ExportTypeCSV',
+	'zhvz0020/model/Formatter',
+	"zhyi0010/commons/zhyi0010_0010_commons",
+	'sap/ui/model/Filter'
+], function(Controller, Button, Dialog, MessageToast, Text, TextArea, HorizontalLayout, VerticalLayout, JSONModel, Export, ExportTypeCSV,
+	Formatter, ommons,Filter     ) {
 	"use strict";
 
-var locale = sap.ui.getCore().getConfiguration().getLanguage();
-/*var oBundle = new sap.ui.model.resource.ResourceModel({
+	var locale = sap.ui.getCore().getConfiguration().getLanguage();
+	/*var oBundle = new sap.ui.model.resource.ResourceModel({
 
-			bundleUrl: "/zhyi0010/i18n/i18n.properties",
+				bundleUrl: "/zhyi0010/i18n/i18n.properties",
 
-			locale: locale
+				locale: locale
 
+		});
+	*/
+	var oBundle = jQuery.sap.resources({
+		url: "/zhyi0010/i18n/i18n.properties",
+		locale: locale
 	});
-*/	
-	var oBundle = jQuery.sap.resources({url : "/zhyi0010/i18n/i18n.properties", locale: locale});
-		sap.ui.getCore().setModel(oBundle, "i18n");
+	sap.ui.getCore().setModel(oBundle, "i18n");
 
+	// var path = jQuery.sap.getModulePath("yourComponent", "/zhyi0010/images/hoimi.jpg");
+	// var image = $("#yourImageid");
+	// image.attr("src", path);
 
-// var path = jQuery.sap.getModulePath("yourComponent", "/zhyi0010/images/hoimi.jpg");
-// var image = $("#yourImageid");
-// image.attr("src", path);
-
-// var img = new sap.m.Image({
-//	 src : "/zhyi0010/images/hoimi.jpg"
-// });
+	// var img = new sap.m.Image({
+	//	 src : "/zhyi0010/images/hoimi.jpg"
+	// });
 
 	jQuery.sap.log.setLevel(jQuery.sap.log.Level.ERROR);
 
 	jQuery.sap.log.warning("This should never have happened!");
-	
-/*	var sText = oBundle.getText("appTitle");
-	
-	jQuery.sap.log.error("appTitle : " + sText);
-	
-*/	return Controller.extend("zhvz0020.controller.zhvz0020_0010", {
 
-		onInit: function () {
+	/*	var sText = oBundle.getText("appTitle");
+		
+		jQuery.sap.log.error("appTitle : " + sText);
+		
+	*/
+	return Controller.extend("zhvz0020.controller.zhvz0020_0010", {
+
+		onInit: function() {
 			// set explored app's demo model on this sample
-/*			this.oModel = new JSONModel(jQuery.sap.getModulePath("ZFAV0020.mockdata", "/ZFAV0020.json"));
-			var oView = this.getView();
-			oView.setModel(this.oModel);
-			this.oSF = oView.byId("searchField");
-			// var domId = this.byId("searchField").getId();
-			// $( "#"+domId	 ).click(function() {
-			//	 jQuery.sap.delayedCall(0, this, function() {
-			//			 this.byId("searchField").focus();
-			//	 });
-			// });
-*/
-//			this.getView().byId('__input0').setTooltip("12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678");
-//			this.getView().byId('__label0').setTooltip("12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678");
-//			var oRtt = new sap.ui.commons.RichTooltip( "TextRtt2", {text:"This is a smaller RichTooltip that has only the Text"});
-//						var oVizFrame = this.getView().byId("__text0");
-//			oVizFrame.setTooltip(oRtt);
-/*				this.getView().byId('itf1').setVisible(false);
-				this.getView().byId('itf2').setVisible(false);
-				this.getView().byId('itf3').setVisible(false);*/
-/*		this.getView().byId('hoge').onAfterRendering = function() {
-				if (sap.ui.commons.Tree.prototype.onAfterRendering) {
-					sap.ui.commons.Tree.prototype.onAfterRendering.apply(this, arguments);
-						$("#text01").attr("tabindex", 10);
-						$("#text02").attr("tabindex", 20);
-						$("#text03").attr("tabindex", 30);
-						$("#text04").attr("tabindex", 40);
-				}
-				// Your code...
-		};
-*/
-/*		this.getView().byId('icf0001').addEventDelegate({
-				onAfterRendering: function(){
-						$("#text01").attr("tabindex", 10);
-						$("#text02").attr("tabindex", 20);
-						$("#text03").attr("tabindex", 30);
-						$("#text04").attr("tabindex", 40);
-					
-				}
-			}, this);
-*/
+			/*			this.oModel = new JSONModel(jQuery.sap.getModulePath("ZFAV0020.mockdata", "/ZFAV0020.json"));
+						var oView = this.getView();
+						oView.setModel(this.oModel);
+						this.oSF = oView.byId("searchField");
+						// var domId = this.byId("searchField").getId();
+						// $( "#"+domId	 ).click(function() {
+						//	 jQuery.sap.delayedCall(0, this, function() {
+						//			 this.byId("searchField").focus();
+						//	 });
+						// });
+			*/
+			//			this.getView().byId('__input0').setTooltip("12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678");
+			//			this.getView().byId('__label0').setTooltip("12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678");
+			//			var oRtt = new sap.ui.commons.RichTooltip( "TextRtt2", {text:"This is a smaller RichTooltip that has only the Text"});
+			//						var oVizFrame = this.getView().byId("__text0");
+			//			oVizFrame.setTooltip(oRtt);
+			/*				this.getView().byId('itf1').setVisible(false);
+							this.getView().byId('itf2').setVisible(false);
+							this.getView().byId('itf3').setVisible(false);*/
+			/*		this.getView().byId('hoge').onAfterRendering = function() {
+							if (sap.ui.commons.Tree.prototype.onAfterRendering) {
+								sap.ui.commons.Tree.prototype.onAfterRendering.apply(this, arguments);
+									$("#text01").attr("tabindex", 10);
+									$("#text02").attr("tabindex", 20);
+									$("#text03").attr("tabindex", 30);
+									$("#text04").attr("tabindex", 40);
+							}
+							// Your code...
+					};
+			*/
+			/*		this.getView().byId('icf0001').addEventDelegate({
+							onAfterRendering: function(){
+									$("#text01").attr("tabindex", 10);
+									$("#text02").attr("tabindex", 20);
+									$("#text03").attr("tabindex", 30);
+									$("#text04").attr("tabindex", 40);
+								
+							}
+						}, this);
+			*/
 			// set explored app's demo model on this sample
 			// var oModel = new sap.ui.model.json.JSONModel();
 			// oModel.loadData("mockdata/products.json");
@@ -112,14 +117,14 @@ var locale = sap.ui.getCore().getConfiguration().getLanguage();
 		},
 
 		// 得意先サジェスト
-		onSuggest: function (event) {
-//			this.byId("searchField").attr("enableSuggestions", "false");
+		onSuggest: function(event) {
+			//			this.byId("searchField").attr("enableSuggestions", "false");
 			var value = event.getParameter("suggestValue");
-			if (!value) value='　**';
+			if (!value) value = '　**';
 			if (value === '*') value = '';
 			var filters = [];
 			if (value) {
-//			if (!value) value = "					 ";
+				//			if (!value) value = "					 ";
 				filters = [new sap.ui.model.Filter([
 					new sap.ui.model.Filter("Key", function(sText) {
 						return (sText || "").toUpperCase().indexOf(value.toUpperCase()) > -1;
@@ -129,27 +134,27 @@ var locale = sap.ui.getCore().getConfiguration().getLanguage();
 					}),
 					new sap.ui.model.Filter("Tokuisaki", function(sPrefectures) {
 						return (sPrefectures || "").toUpperCase().indexOf(value.toUpperCase()) > -1;
-//						return (!(sPrefectures || ""));
+						//						return (!(sPrefectures || ""));
 					})
 				], false)];
-			// } else {
-			//	this.oSF.getBinding("suggestionItems").filter('');
-			//	this.oSF.suggest();
-			 }
+				// } else {
+				//	this.oSF.getBinding("suggestionItems").filter('');
+				//	this.oSF.suggest();
+			}
 			this.oSF.getBinding("suggestionItems").filter(filters);
 			this.oSF.suggest(event);
-//		var cocdInput = event.getSource();	
-//				console.log(event.getSource().getBinding("suggestionItems").oLastContextData);	
-// this methods call trigger new suggestion item displayed	
-//cocdInput.setShowSuggestion(true);	
-//				cocdInput.setFilterSuggests(false);	 
-//				cocdInput.removeAllSuggestionItems();	 
+			//		var cocdInput = event.getSource();	
+			//				console.log(event.getSource().getBinding("suggestionItems").oLastContextData);	
+			// this methods call trigger new suggestion item displayed	
+			//cocdInput.setShowSuggestion(true);	
+			//				cocdInput.setFilterSuggests(false);	 
+			//				cocdInput.removeAllSuggestionItems();	 
 
-//			this.oSF.liveChange(event);
-//			this.oSF.focus();
+			//			this.oSF.liveChange(event);
+			//			this.oSF.focus();
 		},
 		handleTableSelectDialogPress: function(oEvent) {
-			if (! this._oDialog) {
+			if (!this._oDialog) {
 				this._oDialog = sap.ui.xmlfragment("zhvz0020.view.Dialog", this);
 			}
 
@@ -167,55 +172,61 @@ var locale = sap.ui.getCore().getConfiguration().getLanguage();
 			jQuery.sap.syncStyleClass("sapUiSizeCompact", this.getView(), this._oDialog);
 			this._oDialog.open();
 		},
+		//----------------------------↓ここから↓----------------------------
+		handleTableSelectDialogPress2: function(oEvent) {
+			if (!this._oDialog2) {
+				this._oDialog2 = sap.ui.xmlfragment("zhvz0020.view.Dialog2", this);
+			}
 
-		onApproveDialog: function () {
-			var dialog = new Dialog({
-				title: 'Confirm',
-				type: 'Message',
-				content: new Text({ text: 'Are you sure you want to submit your shopping cart?' }),
-				beginButton: new Button({
-					text: 'Submit',
-					press: function () {
-						MessageToast.show('Submit pressed!');
-						dialog.close();
-					}
-				}),
-				endButton: new Button({
-					text: 'Cancel',
-					press: function () {
-						dialog.close();
-					}
-				}),
-				afterClose: function() {
-					dialog.destroy();
-				}
-			});
- 
-			dialog.open();
+			// Multi-select if required
+			var bMultiSelect = !!oEvent.getSource().data("multi");
+			this._oDialog2.setMultiSelect(bMultiSelect);
+
+			// Remember selections if required
+			var bRemember = !!oEvent.getSource().data("remember");
+			this._oDialog2.setRememberSelections(bRemember);
+
+			this.getView().addDependent(this._oDialog2);
+
+			// clear the old search filter	追加？？？？？
+			this._oDialog2.getBinding("items").filter([]);
+
+			// toggle compact style
+			jQuery.sap.syncStyleClass("sapUiSizeCompact", this.getView(), this._oDialog2);
+			this._oDialog2.open();
+					},
+		handleSearch: function(oEvent) {
+			var sValue = oEvent.getParameter("value");
+			var oFilter = new Filter("MATNR", sap.ui.model.FilterOperator.Contains, sValue);
+			var oBinding = oEvent.getSource().getBinding("items");
+			oBinding.filter([oFilter]);
 		},
+		//----------------------------↑ここまで↑----------------------------
 		onAfterRendering: function() {
 
-						$("div" + "[id$=text01]").attr("tabindex", 10);
-						$("div" + "[id$=text02]").attr("tabindex", 20);
-						$(":input" + "[id*=text03]").attr("tabindex", 30);
-						$(":input" + "[id*=text04]").attr("tabindex", 40);
+			$("div" + "[id$=text01]").attr("tabindex", 10);
+			$("div" + "[id$=text02]").attr("tabindex", 20);
+			$(":input" + "[id*=text03]").attr("tabindex", 30);
+			$(":input" + "[id*=text04]").attr("tabindex", 40);
 		},
-		
-		onApproveDialog: function () {
+
+		onApproveDialog: function() {
 			var dialog = new Dialog({
 				title: 'Confirm',
 				type: 'Message',
-				content: new Text({ text: 'Are you sure you want to submit your shopping cart?' }),
+				content: new Text({
+					text: 'Are you sure you want to submit your shopping cart?'
+				}),
 				beginButton: new Button({
 					text: 'Submit',
-					press: function () {
+					press: function() {
 						MessageToast.show('Submit pressed!');
 						dialog.close();
 					}
 				}),
 				endButton: new Button({
 					text: 'Cancel',
-					press: function () {
+					press: function() {
 						dialog.close();
 					}
 				}),
@@ -223,16 +234,17 @@ var locale = sap.ui.getCore().getConfiguration().getLanguage();
 					dialog.destroy();
 				}
 			});
- 
 			dialog.open();
 		},
- 
-		onRejectDialog: function () {
+
+		onRejectDialog: function() {
 			var dialog = new Dialog({
 				title: 'Reject',
 				type: 'Message',
 				content: [
-					new Text({ text: 'Are you sure you want to reject your shopping cart?' }),
+					new Text({
+						text: 'Are you sure you want to reject your shopping cart?'
+					}),
 					new TextArea('rejectDialogTextarea', {
 						width: '100%',
 						placeholder: 'Add note (optional)'
@@ -240,7 +252,7 @@ var locale = sap.ui.getCore().getConfiguration().getLanguage();
 				],
 				beginButton: new Button({
 					text: 'Reject',
-					press: function () {
+					press: function() {
 						var sText = sap.ui.getCore().byId('rejectDialogTextarea').getValue();
 						MessageToast.show('Note is: ' + sText);
 						dialog.close();
@@ -248,7 +260,7 @@ var locale = sap.ui.getCore().getConfiguration().getLanguage();
 				}),
 				endButton: new Button({
 					text: 'Cancel',
-					press: function () {
+					press: function() {
 						dialog.close();
 					}
 				}),
@@ -256,21 +268,23 @@ var locale = sap.ui.getCore().getConfiguration().getLanguage();
 					dialog.destroy();
 				}
 			});
- 
+
 			dialog.open();
 		},
- 
-		onSubmitDialog: function () {
+
+		onSubmitDialog: function() {
 			var dialog = new Dialog({
 				title: 'Confirm',
 				type: 'Message',
 				content: [
-					new Text({ text: 'Are you sure you want to submit your shopping cart?' }),
+					new Text({
+						text: 'Are you sure you want to submit your shopping cart?'
+					}),
 					new TextArea('submitDialogTextarea', {
 						liveChange: function(oEvent) {
 							var sText = oEvent.getParameter('value');
 							var parent = oEvent.getSource().getParent();
- 
+
 							parent.getBeginButton().setEnabled(sText.length > 0);
 						},
 						width: '100%',
@@ -280,7 +294,7 @@ var locale = sap.ui.getCore().getConfiguration().getLanguage();
 				beginButton: new Button({
 					text: 'Submit',
 					enabled: false,
-					press: function () {
+					press: function() {
 						var sText = sap.ui.getCore().byId('submitDialogTextarea').getValue();
 						MessageToast.show('Note is: ' + sText);
 						dialog.close();
@@ -288,7 +302,7 @@ var locale = sap.ui.getCore().getConfiguration().getLanguage();
 				}),
 				endButton: new Button({
 					text: 'Cancel',
-					press: function () {
+					press: function() {
 						dialog.close();
 					}
 				}),
@@ -296,11 +310,11 @@ var locale = sap.ui.getCore().getConfiguration().getLanguage();
 					dialog.destroy();
 				}
 			});
- 
+
 			dialog.open();
 		},
- 
-		onConfirmDialog: function () {
+
+		onConfirmDialog: function() {
 			var dialog = new Dialog({
 				title: 'Confirm',
 				type: 'Message',
@@ -310,16 +324,28 @@ var locale = sap.ui.getCore().getConfiguration().getLanguage();
 							new VerticalLayout({
 								width: '120px',
 								content: [
-									new Text({ text: 'Type: ' }),
-									new Text({ text: 'Delivery:' }),
-									new Text({ text: 'Items count: ' })
+									new Text({
+										text: 'Type: '
+									}),
+									new Text({
+										text: 'Delivery:'
+									}),
+									new Text({
+										text: 'Items count: '
+									})
 								]
 							}),
 							new VerticalLayout({
 								content: [
-									new Text({ text: 'Shopping Cart' }),
-									new Text({ text: 'Jun 26, 2013' }),
-									new Text({ text: '2' })
+									new Text({
+										text: 'Shopping Cart'
+									}),
+									new Text({
+										text: 'Jun 26, 2013'
+									}),
+									new Text({
+										text: '2'
+									})
 								]
 							})
 						]
@@ -331,7 +357,7 @@ var locale = sap.ui.getCore().getConfiguration().getLanguage();
 				],
 				beginButton: new Button({
 					text: 'Submit',
-					press: function () {
+					press: function() {
 						var sText = sap.ui.getCore().byId('confirmDialogTextarea').getValue();
 						MessageToast.show('Note is: ' + sText);
 						dialog.close();
@@ -339,7 +365,7 @@ var locale = sap.ui.getCore().getConfiguration().getLanguage();
 				}),
 				endButton: new Button({
 					text: 'Cancel',
-					press: function () {
+					press: function() {
 						dialog.close();
 					}
 				}),
@@ -347,7 +373,7 @@ var locale = sap.ui.getCore().getConfiguration().getLanguage();
 					dialog.destroy();
 				}
 			});
- 
+
 			dialog.open();
 		},
 		toggleFullScreen: function() {
@@ -365,72 +391,76 @@ var locale = sap.ui.getCore().getConfiguration().getLanguage();
 		_openFullScreen: function() {
 			var s2Controller = this.oApplicationFacade.getApplicationModel("sharedData").getData().s2Controller;
 			var masterPage = s2Controller.byId("page").getParent().getParent().$();
-			masterPage.css({display: "none"});
+			masterPage.css({
+				display: "none"
+			});
 		},
 
 		_closeFullScreen: function() {
 			var s2Controller = this.oApplicationFacade.getApplicationModel("sharedData").getData().s2Controller;
 			var masterPage = s2Controller.byId("page").getParent().getParent().$();
-			masterPage.css({display: ""});
+			masterPage.css({
+				display: ""
+			});
 		},
-		onDataExport : sap.m.Table.prototype.exportData || function(oEvent) {
+		onDataExport: sap.m.Table.prototype.exportData || function(oEvent) {
 
 			var oExport = new Export({
 
 				// Type that will be used to generate the content. Own ExportType's can be created to support other formats
-				exportType : new ExportTypeCSV({
-					separatorChar : ","
+				exportType: new ExportTypeCSV({
+					separatorChar: ","
 				}),
 
 				// Pass in the model created above
-				models : this.getView().getModel(),
+				models: this.getView().getModel(),
 
 				// binding information for the rows aggregation
-				rows : {
-					path : "/Meisai"
+				rows: {
+					path: "/Meisai"
 				},
 
 				// column definitions with column name and binding info for the content
 
-				columns : [{
-					name : "POSNR",
-					template : {
-						content : "{POSNR}"
+				columns: [{
+					name: "POSNR",
+					template: {
+						content: "{POSNR}"
 					}
 				}, {
-					name : "ZZITEM",
-					template : {
-						content : "{ZZITEM}"
+					name: "ZZITEM",
+					template: {
+						content: "{ZZITEM}"
 					}
 				}, {
-					name : "ZZMAKER",
-					template : {
-						content : "{ZZMAKER}"
+					name: "ZZMAKER",
+					template: {
+						content: "{ZZMAKER}"
 					}
 				}, {
-					name : "ZZSIZE",
-					template : {
-						content : "{ZZSIZE}"
+					name: "ZZSIZE",
+					template: {
+						content: "{ZZSIZE}"
 					}
 				}, {
-					name : "ZZSIZEUNIT",
-					template : {
-						content : "{ZZSIZEUNIT}"
+					name: "ZZSIZEUNIT",
+					template: {
+						content: "{ZZSIZEUNIT}"
 					}
 				}, {
-					name : "LGORT",
-					template : {
-						content : "{LGORT}"
+					name: "LGORT",
+					template: {
+						content: "{LGORT}"
 					}
 				}, {
-					name : "CHARG",
-					template : {
-						content : "{CHARG}"
+					name: "CHARG",
+					template: {
+						content: "{CHARG}"
 					}
 				}, {
-					name : "KWMENG",
-					template : {
-						content : "{KWMENG}"
+					name: "KWMENG",
+					template: {
+						content: "{KWMENG}"
 					}
 				}]
 			});
